2c74998dbbd66e8506f2c4c6ad6b86eb3b9e8d7c
ShahedAlmoslami <shahedalmosalami@gmail.com>
2026-01-24 01:34:47 -0800

    update project files

---
+++ taest/flutter_application_1/lib/ligic/cubit.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_application_1/data/services/repositories.dart';
+import 'package:flutter_application_1/ligic/state.dart';
+
+class AdminCuibt{
+ final Repositories repo;
+  AdminCuibt(this.repo):super (AdminInitial());
+}
